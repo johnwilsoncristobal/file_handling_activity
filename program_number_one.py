6a00528a69a4d673edcb0a4dbd4bdbc7bbf78804
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-27 19:21:15 +0800

    path for number.txt

--- program_number_one.py
+++ program_number_one.py
@@ -1,6 +1,6 @@
 def save_even_and_odd():
 # Read numbers from number.txt
-    with(open("numbers.txt", "r")) as source_file:
+    with(open("./numbers.txt", "r")) as source_file:
         all_numbers = [int(line.strip()) for line in source_file]      
 # Write the even numbers
     with open("even_numbers.txt", "w") as even_file:

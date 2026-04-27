a653a050c28bfc0575eb76e21f10f90f5f7a0a57
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-27 17:12:40 +0800

    write even numbers to even_numbers.txt

--- program_number_one.py
+++ program_number_one.py
@@ -1,4 +1,9 @@
 def save_even_and_odd():
 # Read numbers from number.txt
     with(open("numbers.txt", "r")) as source_file:
-        all_numbers = [int(line.strip()) for line in source_file]
+        all_numbers = [int(line.strip()) for line in source_file]      
+# Write the even numbers
+    with open("even_numbers.txt", "w") as even_file:
+        for number in all_numbers:
+            if number % 2 == 0:
+                even_file.write(str(number) + "\n")

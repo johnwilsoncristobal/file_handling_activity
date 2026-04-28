b275c8220177b6a2dd49d0e32158114b5e134316
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 05:30:20 +0800

    print name and GWA of highest student

--- program_number_two.py
+++ program_number_two.py
@@ -5,3 +5,5 @@ with open("./students.txt", "r") as source_file:
     students = [(name, float(gwa)) for name, gwa in records]
 # Find the student with the highest GWA
     highest_student = max(students, key=lambda x: x[1])
+# Print the result
+    print("Highest GWA:", highest_student[0], highest_student[1])

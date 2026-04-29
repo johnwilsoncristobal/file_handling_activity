46f885ac05c872579c6dd762bb9e30cb5916e151
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 13:10:12 +0800

    Minor addition to the code

--- program_number_two.py
+++ program_number_two.py
@@ -1,5 +1,5 @@
 # Open student.txt and read all lines
-with open("./students.txt", "r") as source_file:
+with (open("./students.txt", "r")) as source_file:
     records = [line.strip().split() for line in source_file]
 # Convert the records into tuple
     students = [(name, float(gwa)) for name, gwa in records]

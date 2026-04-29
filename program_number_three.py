8dfc0aae647d8f91e1129b10f552da98d76ca985
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 11:58:24 +0800

    add loop to allow multiple lines input

--- program_number_three.py
+++ program_number_three.py
@@ -2,3 +2,11 @@ with open("./mylife.txt", "w") as life_file:
 # Ask for first line
     line = input("Enter line: ")
     life_file.write(line + "\n")
+# Loop until it says no
+    while True:
+        choice = input("Are there more lines y/n? ")
+        if choice.lower() == "y":
+            line = input("Enter line: ")
+            life_file.write(line + "\n")
+        else:
+            break

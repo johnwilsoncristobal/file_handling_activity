90dcbac084a60b7c43f3c9644277ebe46e234d48
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 13:07:57 +0800

    Improved parts of code

--- program_number_one.py
+++ program_number_one.py
@@ -2,12 +2,12 @@
 with(open("./numbers.txt", "r")) as source_file:
         all_numbers = [int(line.strip()) for line in source_file]      
 # Write the even numbers
-with open("even.txt", "w") as even_file:
+with (open("even.txt", "w")) as even_file:
         for number in all_numbers:
             if number % 2 == 0:
                 even_file.write(str(number) + "\n")
 #Write the odd numbers
-with open("odd.txt", "w") as odd_file:
+with (open("odd.txt", "w")) as odd_file:
         for number in all_numbers:
             if number % 2 != 0:
                 odd_file.write(str(number) + "\n")

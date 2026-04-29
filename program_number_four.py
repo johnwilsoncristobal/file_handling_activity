38d1d9b20884d162ea5e82b768441580bed260c6
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 13:20:19 +0800

    write squares of even numbers to double.txt and a minor addition added to the first line

--- program_number_four.py
+++ program_number_four.py
@@ -1,3 +1,8 @@
 # Read integers from integers.txt
-with (open("integers.txt", "r")) as source_file:
+with (open("./integers.txt", "r")) as source_file:
     numbers = [int(line.strip()) for line in source_file]
+# Write squares of even numbers to double.txt
+with (open("double.txt", "w")) as double_file:
+    for num in numbers:
+        if num % 2 == 0:
+            double_file.write(str(num ** 2) + "\n")

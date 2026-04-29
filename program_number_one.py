9a53804f75b73421e1dc1e19ddb892f2a589ab00
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-04-29 12:09:46 +0800

    removed the defintion function and improved code

--- program_number_one.py
+++ program_number_one.py
@@ -1,16 +1,13 @@
-def save_even_and_odd():
 # Read numbers from number.txt
-    with(open("./numbers.txt", "r")) as source_file:
+with(open("./numbers.txt", "r")) as source_file:
         all_numbers = [int(line.strip()) for line in source_file]      
 # Write the even numbers
-    with open("even_numbers.txt", "w") as even_file:
+with open("even.txt", "w") as even_file:
         for number in all_numbers:
             if number % 2 == 0:
                 even_file.write(str(number) + "\n")
 #Write the odd numbers
-    with open("odd_numbers.txt", "w") as odd_file:
+with open("odd.txt", "w") as odd_file:
         for number in all_numbers:
             if number % 2 != 0:
                 odd_file.write(str(number) + "\n")
-# Run the function
-save_even_and_odd()
